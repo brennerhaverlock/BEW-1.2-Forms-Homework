4879ac807c9185e2ebc51a8b7ce5cba62f4ce6ad
Brenner Haverlock <80720533+theoneking01@users.noreply.github.com>
2021-04-26 16:56:58 -0700

    completed forms

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -6,20 +6,17 @@ from wtforms.validators import DataRequired, Length, URL
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
 
-    # TODO: Add the following fields to the form class:
-    # - title - StringField
-    # - address - StringField
-    # - submit button
-    pass
+    title = StringField("Title")
+    address = StringField("Address")
+    submit = SubmitField("Submit")
+    
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
 
-    # TODO: Add the following fields to the form class:
-    # - name - StringField
-    # - price - FloatField
-    # - category - SelectField (specify the 'choices' param)
-    # - photo_url - StringField (use a URL validator)
-    # - store - QuerySelectField (specify the `query_factory` param)
-    # - submit button
-    pass
+    name = StringField('Name')
+    price = FloatField('Price')
+    category = SelectField('Category', choices=ItemCategory.choices())
+    photo_url = StringField('Photo URL')
+    store = QuerySelectField('Store', query_factory=lambda: GroceryStore.query.all())
+    submit = SubmitField('Submit')
