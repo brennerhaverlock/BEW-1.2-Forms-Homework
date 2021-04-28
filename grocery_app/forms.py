e0aaa2fe48f0e81a7bf530a470924f9279b7e228
Brenner Haverlock <80720533+theoneking01@users.noreply.github.com>
2021-04-28 07:06:38 -0700

    added login and signup routes

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -57,3 +57,21 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f'{self.username}'
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
